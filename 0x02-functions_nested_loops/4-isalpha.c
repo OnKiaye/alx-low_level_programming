be8e88c41d42de07384d7c40f085c1545ab3a864
OnKiaye <onkiaye@gmail.com>
2022-04-12 07:04:40 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
- * Return: Always 0 (Success)
+ *@c: The int to print
+ * Return: Always 0.
  */
 
 int _isalpha(int c)
