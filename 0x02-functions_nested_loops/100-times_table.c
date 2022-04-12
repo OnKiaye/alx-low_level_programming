807aebcf610ac48a9e479e140f9a5218d7d4b730
OnKiaye <onkiaye@gmail.com>
2022-04-12 08:16:19 -0700

    12.  prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*print_times_table - prints the  times table with
+*times_table - prints the  times table with
 *parameter
 *@n: parameter
 *Return: returns nothing
 */
-void print_times_table(int n)
+void times_table(int n)
 {
 int digit, mult, result;
 if (n <= 15 && n >= 0)
