a4cb961d45bb48b24a605d4ecc36865d5eed7325
OnKiaye <51336041+OnKiaye@users.noreply.github.com>
2022-05-24 13:09:20 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-
+#include "lists.h"
 /**
- * bmain - function executed before main
- * Return: no return.
+ * before_main - function executed before main
+ * Return: void
  */
-
-void __attribute__((constructor)) bmain()
+void before_main(void)
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
